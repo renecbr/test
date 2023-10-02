b560ceb7911650771ca2da40be68e0ad600b9267
renecbr <renecbr1993@gmail.com>
2023-10-02 15:32:29 +0900

    Add files via upload

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -15,6 +15,6 @@ def students_list(request):
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # ordering = 'group'
 
     return render(request, template, context)
