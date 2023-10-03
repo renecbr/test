e80d288e6239abf4a8b8aaf2ae9749a8258a486f
renecbr <renecbr1993@gmail.com>
2023-10-03 13:47:57 +0900

    Add files via upload

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -22,7 +22,6 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
-        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -32,3 +31,6 @@ class Scope(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', default="")
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', default="")
     is_main = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['is_main', 'tag']
